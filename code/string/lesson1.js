1a8a24780a49ccd0ead54992ae2b317bbf802553
luichooy <luichooy@163.com>
2019-08-06 11:29:23 +0800

    557. 反转字符串中的单词 其他解法

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,6 +1,30 @@
+// export default str => {
+//   return str
+//     .split(' ')
+//     .map(word => {
+//       return word
+//         .split('')
+//         .reverse()
+//         .join('')
+//     })
+//     .join(' ')
+// }
+
+// export default str => {
+//   return str
+//     .split(/\s/g)
+//     .map(word => {
+//       return word
+//         .split('')
+//         .reverse()
+//         .join('')
+//     })
+//     .join(' ')
+// }
+
 export default str => {
   return str
-    .split(' ')
+    .match(/[\w']+/g)
     .map(word => {
       return word
         .split('')
